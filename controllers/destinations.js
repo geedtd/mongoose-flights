77d0d698a65f6fa7a07795bef2f42b3658010721
geedtd <grcazares@gmail.com>
2021-07-16 03:24:42 -0700

    added form on destination.new.ejs

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -2,8 +2,15 @@ import { Destination } from '../models/destination.js'
 
 export {
     newDestination as new,
+    
 }
 
-function newDestination ( req,res ){
-    console.log('YOOOOOOOOOO')
+function newDestination ( req, res ){
+    Destination.find({}, function(err, destinations) {
+        res.render('destinations/new', {
+            destinations: destinations,
+            err: err,
+        })
+    })
+    
 }
